15d701c0d60ebcdffed835adb7aba8a69b70c4fd
anishjose27 <80158428+anishjose27@users.noreply.github.com>
2021-03-07 12:51:19 +0530

    Add files via upload

---
+++ src/assets/ckeditor/plugins/deletebutton/plugin.js
@@ -0,0 +1,23 @@
+﻿CKEDITOR.plugins.add('deletebutton',
+{
+    init: function (editor) {
+
+       
+        editor.ui.addButton("deletebutton",
+        {
+            label: 'Delete textbox',
+            command: 'delete',
+            icon: this.path + "Deletebutton.png"
+        });
+        var cmd = editor.addCommand('delete', { exec: showDeleteMessage });
+        var defaultConfig = {
+            deletefunction: function(currentLength, maxLength) {
+                dispatchEvent('deletefunction', currentLength, maxLength);
+            },
+        }
+        var config = CKEDITOR.tools.extend(defaultConfig, editor.config.deletebutton || {}, true);
+    }
+});
+function showDeleteMessage(e) {
+    CKEDITOR.currentInstance.config.deletebutton.deletefunction();
+}
